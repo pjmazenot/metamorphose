7fde3d8a01b3ef303ecf11016809723ebf7cb4b0
Pierre-Julien MAZENOT <pj.mazenot@gmail.com>
2020-01-25 03:05:36 -0500

    Add tests for xml processing

---
+++ tests/codeception/functional/Advanced/Formats/XmlAdvancedCest.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * This file is part of the Metamorphose package.
+ *
+ * Copyright (c) 2020 Pierre-Julien Mazenot <pj.mazenot@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tests\Codeception\Functional\Basic\Formats;
+
+use Tests\Codeception\FunctionalTester;
+use Tests\Codeception\TestCase\BaseFunctionalTest;
+
+class XmlAdvancedCest extends BaseFunctionalTest {
+
+//    public function testCustomAttributes(FunctionalTester $I) {
+//
+//        $this->runXmlTest($I, 'advanced-xml/custom-attributes');
+//
+//    }
+
+    public function testCustomProlog(FunctionalTester $I) {
+
+        $this->runXmlTest($I, 'advanced-xml/custom-prolog');
+
+    }
+
+    public function testOutputCdata(FunctionalTester $I) {
+
+        $this->runXmlTest($I, 'advanced-xml/output-cdata');
+
+    }
+
+}
