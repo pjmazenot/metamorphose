7de64654067d227ca62e82bc01cbdd8408654ce7
Pierre-Julien MAZENOT <pj.mazenot@gmail.com>
2020-01-25 06:25:01 -0500

    Add database source and destination (PDO)

--- src/Input/DataSources/DatabaseDataSource.php
+++ src/Input/DataSources/DatabaseDataSource.php
@@ -25,39 +25,75 @@ class DatabaseDataSource extends DataSource {
 
     const TYPE = 'database';
 
-    /** @var \PDO $connection */
-    protected $connection;
-
-    /**
-     * DatabaseDataSource constructor.
-     *
-     * @param string $dsn
-     * @param string $user
-     * @param string $password
-     * @param array  $options
-     */
-    public function __construct(string $dsn, string $user, string $password, array $options = []) {
-
-        // Init the connection
-        $this->connection = new \PDO($dsn, $user, $password, $options);
-
-    }
-
     /**
      * Extract the content from a string
      *
-     * @param array|string         $sourceData TBD
      * @param ContractSourceDefinition $sourceDefinition
      * @param ParserInterface|null $parser     Optional parser name
      *
      * @throws MetamorphoseDataSourceException
      * @throws MetamorphoseParserException
      */
-    public function extract($sourceData, ContractSourceDefinition $sourceDefinition, ?ParserInterface $parser = null): void {
+    public function extract(ContractSourceDefinition $sourceDefinition, ?ParserInterface $parser = null): void {
 
-        // Get PDO connection string
+        $options = $sourceDefinition->getOptions();
+        
+        if(empty($options['connection']['dsn'])) {
 
-        // @TODO: Handle PDO queries
+            throw $this->getException('No connection info provided');
+
+        }
+
+        if(empty($options['query'])) {
+
+            throw $this->getException('No query provided');
+
+        }
+
+        if(empty($options['params'])) {
+
+            $options['params'] = [];
+
+        }
+
+        // Init the connection
+        $connection = new \PDO(
+            $options['connection']['dsn'],
+            isset($options['connection']['username']) ? $options['connection']['username'] : null,
+            isset($options['connection']['password']) ? $options['connection']['password'] : null,
+            isset($options['connection']['options']) ? $options['connection']['options'] : null
+        );
+
+        // Prepare the query
+        $prep = $connection->prepare($options['query']);
+
+        // Execute the query
+        $isExecuted = $prep->execute($options['params']);
+
+        if($isExecuted === false) {
+
+            throw $this->getException('The query couldn\'t be executed, reason: "' . $prep->errorInfo()[2] . '" (' . $options['query'] . ' - array(' . implode(', ', $valueArray) . '))');
+
+        }
+
+        $results = $prep->fetchAll(\PDO::FETCH_ASSOC);
+
+        if(empty($results)) {
+
+            throw $this->getException('No result for the query');
+
+        }
+
+        if($sourceDefinition->getType() === ContractSourceDefinition::STRUCTURE_OBJECT) {
+
+            // @TODO: Should we send a warning when there is more than one result?
+            $this->data = $parser->parse($results[0], $sourceDefinition->getOptions());
+
+        } else {
+
+            $this->data = $parser->parse($results[0], $sourceDefinition->getOptions());
+
+        }
 
     }
 
--- src/Output/DataDestinations/DatabaseDataDestination.php
+++ src/Output/DataDestinations/DatabaseDataDestination.php
@@ -13,7 +13,6 @@ namespace Metamorphose\Output\DataDestinations;
 use Metamorphose\Contract\Definitions\ContractDestinationDefinition;
 use Metamorphose\Data\DataSet;
 use Metamorphose\Exceptions\MetamorphoseDataDestinationException;
-use Metamorphose\Exceptions\MetamorphoseFormatterException;
 use Metamorphose\Output\DataDestination;
 use Metamorphose\Output\FormatterInterface;
 
@@ -33,13 +32,77 @@ class DatabaseDataDestination extends DataDestination {
      * @param ContractDestinationDefinition $destinationDefinition
      * @param FormatterInterface|null       $formatter
      *
-     * @return mixed|string
+     * @return mixed
      * @throws MetamorphoseDataDestinationException
-     * @throws MetamorphoseFormatterException
      */
     public function load(DataSet $finalDataSet, ContractDestinationDefinition $destinationDefinition, ?FormatterInterface $formatter) {
 
-        // @TODO: Handle PDO queries
+        $dataArray = $finalDataSet->getData()->toArray();
+        $options = $destinationDefinition->getOptions();
+
+        if(empty($options['connection']['dsn'])) {
+
+            throw $this->getException('No connection info provided');
+
+        }
+
+        if(empty($options['table_name'])) {
+
+            throw $this->getException('No table name provided');
+
+        }
+
+        // Init the connection
+        $connection = new \PDO(
+            $options['connection']['dsn'],
+            isset($options['connection']['username']) ? $options['connection']['username'] : null,
+            isset($options['connection']['password']) ? $options['connection']['password'] : null,
+            isset($options['connection']['options']) ? $options['connection']['options'] : null
+        );
+
+        $fieldNames = [];
+        foreach($destinationDefinition->getFields() as $field) {
+
+            $fieldNames[] = $field->getName();
+
+        }
+
+        $query = 'INSERT INTO ' . $options['table_name'] . ' (' . implode(', ', $fieldNames) . ') VALUES ';
+        $valuesTemplate = '(' . implode(', ', array_fill(0, count($fieldNames), '?')) . ')';
+
+        if($destinationDefinition->getType() === ContractDestinationDefinition::STRUCTURE_OBJECT) {
+
+            $values = $valuesTemplate;
+            $params = $dataArray;
+
+        } else {
+
+            $values = [];
+            $params = [];
+            foreach($dataArray as $dataItem) {
+
+                $values[] = $valuesTemplate;
+                $params = array_merge($params, array_values($dataItem));
+
+            }
+
+            $values = implode(', ', $values);
+
+        }
+
+        // Prepare the query
+        $prep = $connection->prepare($query . $values);
+
+        // Execute the query
+        $isExecuted = $prep->execute($params);
+
+        if($isExecuted === false) {
+
+            throw $this->getException('The query couldn\'t be executed, reason: "' . $prep->errorInfo()[2] . '" (' . $query . $values . ' - array(' . implode(', ', $params) . '))');
+
+        }
+
+        return $isExecuted;
 
     }
 
